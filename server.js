43da08df263811f05e65d823c6b2c62a7e948f3e
Blaine Curren <blaine.curren@gmail.com>
2021-07-22 08:42:10 -0500

    Added a candidate route to change their party

--- server.js
+++ server.js
@@ -100,6 +100,37 @@ app.post("/api/candidate", ({ body }, res) => {
   });
 });
 
+// Update a candidate's party
+app.put("/api/candidate/:id", (req, res) => {
+  // Forces any put request to /api/candidate/:id to include a party_id property
+  const errors = inputCheck(req.body, "party_id");
+
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `UPDATE candidates SET party_id = ?
+        WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // check if a record was found
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "Candidate not found",
+      });
+    } else {
+      res.json({
+        message: "success",
+        data: req.body,
+        changes: result.affectedRows,
+      });
+    }
+  });
+});
+
 // Delete a candidate
 app.delete("/api/candidate/:id", (req, res) => {
   const sql = `DELETE FROM candidates WHERE id = ?`;
