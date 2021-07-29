d14f5539a79d106b9be79910dbd38e8bf5c6761f
Blaine Curren <blaine.curren@gmail.com>
2021-07-28 19:33:46 -0500

    added PUT voterRoutes.js

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -37,6 +37,7 @@ router.get("/voter/:id", (req, res) => {
 
 // POST Request
 router.post("/voter", ({ body }, res) => {
+  // Data validation
   const errors = inputCheck(body, "first_name", "last_name", "email");
   if (errors) {
     res.status(400).json({ error: errors });
@@ -56,6 +57,35 @@ router.post("/voter", ({ body }, res) => {
       data: body,
     });
   });
+});
+
+// PUT Route
+router.put('/voter/:id', (req, res) => {
+    // Data validation
+    const errors = inputCheck(req.body, 'email');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `UPDATE voters SET email = ? WHERE id = ?`;
+    const params = [req.body.email, req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Voter not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
 });
 
 module.exports = router;
