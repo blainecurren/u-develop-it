af5cc6c79458761d361e2bce410535e5e7da8779
Blaine Curren <blaine.curren@gmail.com>
2022-11-16 13:48:01 -0600

    added delete route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,86 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "Jojo2727",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+// Get all candidates
+app.get("/api/candidates", (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
+// GET a single candidate
+app.get("/api/candidate/:id", (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+});
+
+// CREATE a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+        VALUES(?,?,?,?)`;
+const params = [1, "Ronald", "Firbank", 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
+// Delete a candidate
+app.delete("/api/candidate/:id", (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({ message: "Candidate not found" });
+    } else {
+      res.json({
+        message: "Deleted",
+        changes: result.affectedRows,
+        id: req.params.id,
+      });
+    }
+  });
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
