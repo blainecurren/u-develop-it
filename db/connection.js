844270f2c49df32b468332228e49f0ee9b8e3d31
Blaine Curren <blaine.curren@gmail.com>
2022-11-21 08:47:29 -0600

    updated partyRoutes

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,11 @@
 const mysql = require("mysql2");
 
 // Connect to database
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "Jojo2727",
-    database: "election",
-  },
-  console.log("Connected to the election database.")
-);
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Jojo2727",
+  database: "election",
+});
 
 module.exports = db;
--- server.js
+++ server.js
@@ -13,15 +13,16 @@ app.use(express.json());
 
 app.use("/api", apiRoutes);
 
-
-
-
-
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected.");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
